10cce75bf251dfcf71fd813fead761bf74f01a36
dansniper888 <dansniper888@gmail.com>
2024-11-14 14:46:20 +0700

    Update module_1_7.py

--- module_1_7.py
+++ module_1_7.py
@@ -1,4 +1,6 @@
 immutable_var = 1, 2, 'aplle', 2, 'sample'
+# immutable_var .remove(2)
+# AttributeError: 'tuple' object has no attribute 'remove'
 print(immutable_var)
 mutable_list = ['badge', 'clip', 'compass', 'eraser']
 mutable_list .extend(['holder'])
